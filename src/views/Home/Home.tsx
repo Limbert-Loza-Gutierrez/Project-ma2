d1487cf61c93fe8f3bfbf2ea2ec1c5d97d6e0e75
Limbert Eddy Loza Gutierrez <lozagutierrezlimbert@gmail.com>
2024-03-22 19:55:11 -0400

    mejora de estilo modal

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,12 +1,13 @@
+import "./Home.styles.css";
 import logo from '../../../public/image/logo.jpg'
 
 const Home = () => {
   return (
-    <div className='window-content'>
+    <div className='window-content-Home'>
         <img src={logo} alt="" />
         <div className="frase-empresa">
-            <h1>¡Bienvenido a la Clínica Virtual!</h1>
-            <p>La mejor opción para la atención de tus pacientes</p>
+            <h1>¡Bienvenido al Sistema de Apoyo para Psicólogos!</h1>
+            <p>Bienvenido al Sistema de Apoyo para Psicólogos. Nuestro propósito es identificar indicios de maltrato psicológico a través de redes neuronales y métodos de paidografología. Nos enfocamos en analizar el test del "Persona bajo la lluvia", especialmente diseñado para niños de 5 a 12 años.</p>
         </div>
     </div>
   )
