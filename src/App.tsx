5068f1fbbb8512bd33b14b238231fb8f9b8d3d2e
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2024-04-28 00:03:18 -0400

    sulution

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,71 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { Login, Logout,Home} from "./views/exports"
-import {
-  AdministrarPersonal,
-  AdministrarPacientes,
-  NuevoCaso,
-  Casos,
-  PrivateRoute,
-  PublicRoute,
-  Reportes,
-} from "./components/exports"
+// import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// import { Login, Logout,Home} from "./views/exports"
+// import {
+//   AdministrarPersonal,
+//   AdministrarPacientes,
+//   NuevoCaso,
+//   Casos,
+//   PrivateRoute,
+//   PublicRoute,
+//   Reportes,
+// } from "./components/exports"
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <PrivateRoute />,
-    children: [
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <PrivateRoute />,
+//     children: [
 
-      {
-        index: true,
-        element: <Home/>
-      },
-      {
-        path: "/logout",
-        element: <Logout />,
-      },
-      {
-        path: "/administrar-pacientes",
-        element: <AdministrarPacientes />,
-      },
-      {
-        path: "/administrar-personal",
-        element: <AdministrarPersonal />,
-      },
-      {
-        path: "/nuevo-caso",
-        element: <NuevoCaso />,
-      },
-      {
-        path: "/casos",
-        element: <Casos />,
-      },
-      {
-        path: "/reportes",
-        element: <Reportes/>,
-      },
+//       {
+//         index: true,
+//         element: <Home/>
+//       },
+//       {
+//         path: "/logout",
+//         element: <Logout />,
+//       },
+//       {
+//         path: "/administrar-pacientes",
+//         element: <AdministrarPacientes />,
+//       },
+//       {
+//         path: "/administrar-personal",
+//         element: <AdministrarPersonal />,
+//       },
+//       {
+//         path: "/nuevo-caso",
+//         element: <NuevoCaso />,
+//       },
+//       {
+//         path: "/casos",
+//         element: <Casos />,
+//       },
+//       {
+//         path: "/reportes",
+//         element: <Reportes/>,
+//       },
       
-    ],
-  },
-  {
-    path: "/",
-    element: <PublicRoute />,
-    children: [
-      {
-        index: true,
-        element: <h1>Public</h1>,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
+//     ],
+//   },
+//   {
+//     path: "/",
+//     element: <PublicRoute />,
+//     children: [
+//       {
+//         index: true,
+//         element: <h1>Public</h1>,
+//       },
+//       {
+//         path: "/login",
+//         element: <Login />,
+//       },
 
-    ],
-  },
-]);
+//     ],
+//   },
+// ]);
 
-function App() {
-  return <RouterProvider router={router} />;
-}
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
 
-export default App;
+// export default App;
