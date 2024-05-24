5146438785093c8895be19f0a60a97fb948d96be
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2024-05-23 23:28:51 -0400

    update

--- src/components/customs/CustomSidebar/CustomSidebarOptions.tsx
+++ src/components/customs/CustomSidebar/CustomSidebarOptions.tsx
@@ -1,13 +1,25 @@
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { getAuth } from "firebase/auth";
+import { appFirebase } from "../../../config/firebase";
+import { sendPasswordResetEmail } from "firebase/auth";
+const auth = getAuth(appFirebase);
 const CustomSidebarOptions = ({ data_user, opciones }) => {
-  
+  const changePassword = async () => {
+    try {
+      // await sendPasswordResetEmail(auth, data_user.correoInstitucional);
+      await sendPasswordResetEmail(auth, "prueba");
+      alert("Se ha enviado un correo para cambiar la contraseña");
+    } catch (error) {
+      alert("Ha ocurrido un error al enviar el correo");
+    }
+  };
+
   return (
     <aside className="sidebar">
       <div className="perfil-usuario">
         <img src={data_user.imgPerfil} alt="" className="foto-usuario" />
-        {data_user.nivelJerarquico ===
-          "Super Administrador" ? (
+        {data_user.nivelJerarquico === "Super Administrador" ? (
           <h3>Administrador</h3>
         ) : (
           <div className="text-perfil nome-usuario">
@@ -27,6 +39,7 @@ const CustomSidebarOptions = ({ data_user, opciones }) => {
             </Link>
           );
         })}
+        <button onClick={changePassword}>Cambiar Contraseña</button>
       </div>
     </aside>
   );
